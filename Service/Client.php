ef3aa0785833b3e7e1e4355224d099deb39fc78e
Eugeny Fomin <github.com@jeka.ru>
2012-12-08 22:53:26 +0400

    added service

---
+++ Service/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jeka\QiwiBundle\Service;
+use Jeka\QiwiAPI\SOAP\UpdateBillParams;
+
+class Client extends \Jeka\QiwiAPI\SOAP\Client{
+
+    public function updateBill(UpdateBillParams $params)
+    {
+        // TODO: Implement updateBill() method.
+    }
+}
